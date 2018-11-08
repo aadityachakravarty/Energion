a996f119f525298d5d34b9df0303d65e2b283a84
Aaditya Chakravarty <ac36500@gmail.com>
2018-11-08 14:44:30 +0530

    Strip Legacy

--- src/app/consumer/application/new-application/new-application.model.ts
+++
@@ -1,3 +0,0 @@
-export  class NewApplication {
-  constructor(public applicantName: string, public fatherName_husbandName: string, public aadharNumber: number, public mobileNumber: string, public email: string, public permanentAddress: string, public connectionAddress: string, public connectionAddressLandDocument: string, public loadDemand: number, public connectionCategory: string, public connectionType: string, public voltageSupply: string) {}
-}
