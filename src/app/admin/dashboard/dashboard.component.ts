5a878419e52165aa3a7e2e71e6f2734e499fe6a5
Aaditya Chakravarty <ac36500@gmail.com>
2019-03-26 23:00:44 +0530

    MBI #?

--- src/app/admin/dashboard/dashboard.component.ts
+++ src/app/admin/dashboard/dashboard.component.ts
@@ -1,26 +1,23 @@
-import { Component, OnInit, AfterViewChecked } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/auth/auth.service';
-import { TitleService } from 'src/app/title.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent implements OnInit, AfterViewChecked {
+export class DashboardComponent implements OnInit {
 
   profile: any = JSON.parse(sessionStorage.profile);
 
   constructor(
-    private auth: AuthService,
-    private title: TitleService,
+    private auth: AuthService
   ) { }
 
   ngOnInit() {
-    this.auth.getProfile();
-  }
-
-  ngAfterViewChecked() {
+    if (!sessionStorage.profile) {
+      this.auth.getProfile();
+    }
   }
 
   logout() {
